f158f7c575e9833f7baa6b52bffca4b597f87314
derrickkirezi <134754603+derrickkirezi@users.noreply.github.com>
2026-01-25 16:04:38 +0200

    Update maternal_mortality.py

--- notebook/01_datacleaning/maternal_mortality.py
+++ notebook/01_datacleaning/maternal_mortality.py
@@ -2,40 +2,49 @@ import pandas as pd
 
 #1. CREATING A DATAFRAME
 
-df1 = pd.read_csv('data/maternal_mortality.csv', sep=',', skiprows=4)
+df = pd.read_csv('data/maternal_mortality.csv', sep=',', skiprows=4)
 
 #2. TAKING A LOOK AT DATAFRAME
 
-df1.head()
+df.head()
 
-#3. CLEANING DATA (starting by df1)
+#3. CLEANING DATA 
    
-      # A. renaming the columns
+      # renaming the columns
 
-df1= df1.rename(columns={ 'Country Name':'country',
+df= df.rename(columns={ 'Country Name':'country',
                        'Country Code':'country_code',
                        'Indicator Name':'indicator',
                        'Indicator Code':'indicator_code' } )
 
-     # B. identifying the years columns
+     # Identifying the years columns
 
-year_cols = [c for c in df1.columns if c.isdigit()]
+year_cols = [c for c in df.columns if c.isdigit()]
 
-     # C. changing from wide to long format
+     # keep relevant columns
 
-df1.melt(id_vars=['country','country_code','indicator','indicator_code'], var_name='year', value_name='value')
-df1_long= df1.melt(id_vars=['country','country_code','indicator','indicator_code'], var_name='year', value_name='value')
+df = df[["country", "country_code", "indicator"] + year_cols]
 
-    # D. removing null values
+     # changing from wide to long format
 
-df1_long.dropna()
-df1= df1_long.dropna()
+df.melt(id_vars=['country','country_code','indicator','indicator_code'], var_name='year', value_name='value')
+df_long= df.melt(id_vars=['country','country_code','indicator','indicator_code'], var_name='year', value_name='value')
 
-    # E. checking the data types of columns
+    # remove null values
 
-df1.dtypes
+df_long.dropna()
+df= df_long.dropna()
 
-    # F. changing the 'year' column to integer for future calculations
+    # checking the data types of columns
 
-df1['year'].astype(int)
-df1['year']= df1['year'].astype(int)
+df.dtypes
+
+    # changing the 'year' column to integer
+
+df['year'].astype(int)
+df['year']= df['year'].astype(int)
+
+    # save cleaned data
+df_long.to_csv(
+    "data/maternal_mortality_clean.csv",
+    index=False )
