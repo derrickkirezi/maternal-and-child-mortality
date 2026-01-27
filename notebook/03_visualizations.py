0b2d9a1a468dd90abfbdbac6baccb664480f6753
derrickkirezi <134754603+derrickkirezi@users.noreply.github.com>
2026-01-27 11:27:43 +0200

    Update 03_visualizations.py

--- notebook/03_visualizations.py
+++ notebook/03_visualizations.py
@@ -1,36 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# load master dataset
+# load merged dataset
 df = pd.read_csv("data/mortality_merged.csv")
 
-# Filter for Rwanda
-rwanda = df[df["country"] == "Rwanda"].sort_values("year")
+# I'm selecting countries and the under-5 indicator
+countries = ["Rwanda", "Kenya", "Uganda", "Tanzania"]
+df_sub = df[df["country"].isin(countries)].sort_values(["country","year"])
 
-# Creating a plot
-fig, ax1 = plt.subplots()
+plt.figure(figsize=(10,6))
 
-# Left axis: infant & under-5
-ax1.plot(rwanda["year"], rwanda["value_infant"], label="Infant mortality")
-ax1.plot(rwanda["year"], rwanda["under5_mortality"], label="Under-5 mortality")
-ax1.set_xlabel("Year")
-ax1.set_ylabel("Mortality rate (per 1,000 live births)")
+for country in countries:
+    temp = df_sub[df_sub["country"] == country]
+    plt.plot(temp["year"], temp["under5_mortality"], label=country)
 
-# right axis: maternal
-ax2 = ax1.twinx()
-ax2.plot(rwanda["year"], rwanda["value_maternal"], linestyle="--", label="Maternal mortality")
-ax2.set_ylabel("Maternal mortality (per 100,000 live births)")
+plt.xlabel("Year")
+plt.ylabel("Under-5 mortality (per 1,000 live births)")
+plt.title("Under-5 Mortality Trends in East Africa")
+plt.legend()
+plt.grid(True)
 
-# Title
-plt.title("Trends in Maternal and Child Mortality in Rwanda")
-
-# legends
-lines_1, labels_1 = ax1.get_legend_handles_labels()
-lines_2, labels_2 = ax2.get_legend_handles_labels()
-ax1.legend(lines_1 + lines_2, labels_1 + labels_2)
-
-# save figure
-plt.savefig("figures/rwanda_mortality_trends.png", dpi=300, bbox_inches="tight")
+plt.savefig("figures/under5_east_africa.png", dpi=300, bbox_inches="tight")
 plt.close()
 
-print("Saved: figures/rwanda_mortality_trends.png")
+print("Saved: figures/under5_east_africa.png")
