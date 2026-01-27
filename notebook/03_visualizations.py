e4235658b38f359c585f88bfa0df221f7fda9371
derrickkirezi <134754603+derrickkirezi@users.noreply.github.com>
2026-01-27 10:56:14 +0200

    Update 03_visualizations.py

--- notebook/03_visualizations.py
+++ notebook/03_visualizations.py
@@ -8,15 +8,26 @@ df = pd.read_csv("data/mortality_merged.csv")
 rwanda = df[df["country"] == "Rwanda"].sort_values("year")
 
 # Creating a plot
-plt.figure()
-plt.plot(rwanda["year"], rwanda["value_maternal"], label="Maternal mortality")
-plt.plot(rwanda["year"], rwanda["value_infant"], label="Infant mortality")
-plt.plot(rwanda["year"], rwanda["under5_mortality"], label="Under-5 mortality")
+fig, ax1 = plt.subplots()
 
-plt.xlabel("Year")
-plt.ylabel("Mortality rate")
+# Left axis: infant & under-5
+ax1.plot(rwanda["year"], rwanda["value_infant"], label="Infant mortality")
+ax1.plot(rwanda["year"], rwanda["under5_mortality"], label="Under-5 mortality")
+ax1.set_xlabel("Year")
+ax1.set_ylabel("Mortality rate (per 1,000 live births)")
+
+# right axis: maternal
+ax2 = ax1.twinx()
+ax2.plot(rwanda["year"], rwanda["value_maternal"], linestyle="--", label="Maternal mortality")
+ax2.set_ylabel("Maternal mortality (per 100,000 live births)")
+
+# Title
 plt.title("Trends in Maternal and Child Mortality in Rwanda")
-plt.legend()
+
+# legends
+lines_1, labels_1 = ax1.get_legend_handles_labels()
+lines_2, labels_2 = ax2.get_legend_handles_labels()
+ax1.legend(lines_1 + lines_2, labels_1 + labels_2)
 
 # save figure
 plt.savefig("figures/rwanda_mortality_trends.png", dpi=300, bbox_inches="tight")
