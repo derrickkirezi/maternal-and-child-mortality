622a697e9e7bb18d61959c26a1dcae560efc9fec
derrickkirezi <134754603+derrickkirezi@users.noreply.github.com>
2026-01-27 08:06:59 +0200

    Update 02_merge_datasets.py

--- notebook/02_merge_datasets.py
+++ notebook/02_merge_datasets.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-# Load cleaned datasets
+# load cleaned datasets
 maternal = pd.read_csv("data/maternal_mortality_clean.csv")
 infant = pd.read_csv("data/infant_mortality_clean.csv")
 under5 = pd.read_csv("data/under5_mortality_clean.csv")
 
-# Merge step 1: maternal + infant
+# merge step 1: maternal + infant
 df = pd.merge(maternal, infant,
               on=["country","country_code","year"],
               how="outer",
               suffixes=("_maternal","_infant"))
 
-# Merge step 2: add under5
+#Merge step 2: add under5
 df = pd.merge(df, under5,
               on=["country","country_code","year"],
               how="outer")
@@ -19,10 +19,10 @@ df = pd.merge(df, under5,
 # Rename under5 column
 df = df.rename(columns={"value":"under5_mortality"})
 
-# Optional: reorder columns
+# reorder columns
 df = df[["country","country_code","year","value_maternal","value_infant","under5_mortality"]]
 
-# Save master table
+# save master table
 df.to_csv("data/mortality_merged.csv", index=False)
 
 print("Saved: data/mortality_merged.csv")
